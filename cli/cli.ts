6ebc179ea0b344a2c56d03f24796c8e9fb8b1a09
dandolha <dolha.dan@gmail.com>
2025-04-02 12:34:26 +0300

    add prettier

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,159 +1,178 @@
 import { Command } from 'commander';
 import inquirer from 'inquirer';
-import {MusicBrainzApi, CoverArtArchiveApi, IImage} from 'musicbrainz-api';
+import { MusicBrainzApi, CoverArtArchiveApi, IImage } from 'musicbrainz-api';
 import fs from 'fs';
 import { createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import axios from 'axios';
 
 interface Record {
-  artist: string,
-  title: string,
-  publishedAt: string,
-  releaseID?: string,
-  id: number,
+    artist: string;
+    title: string;
+    publishedAt: string;
+    releaseID?: string;
+    id: number;
 }
 
 interface Collection {
-  records: Record[],
-  metadata: {
-    lastUsedID: number,
-  }
+    records: Record[];
+    wishlist: Record[];
+    metadata: {
+        lastUsedID: number;
+    };
 }
 
 const program = new Command();
 
 const mbApi = new MusicBrainzApi({
-  appName: 'vinyls-cli',
-  appVersion: '1.0.0',
-  appContactInfo: 'your@email.com',
+    appName: 'vinyls-cli',
+    appVersion: '1.0.0',
+    appContactInfo: 'your@email.com',
 });
 
 const coverArtApi = new CoverArtArchiveApi();
 
 function readCollection(): Collection {
-  try {
-    return JSON.parse(fs.readFileSync('./src/collections.json', 'utf-8'));
-  }catch{
-    console.log("Couldn't read collection file")
-    return {
-      records:[],
-      metadata: {
-        lastUsedID: -1,
-      }
-    };
-  }
+    try {
+        return JSON.parse(fs.readFileSync('./src/collections.json', 'utf-8'));
+    } catch {
+        console.log("Couldn't read collection file");
+        return {
+            records: [],
+            wishlist: [],
+            metadata: {
+                lastUsedID: -1,
+            },
+        };
+    }
 }
 
 function saveCollection(collection: Collection) {
-  fs.writeFileSync('./src/collections.json', JSON.stringify(collection, null, 2), 'utf-8');
+    fs.writeFileSync('./src/collections.json', JSON.stringify(collection, null, 2), 'utf-8');
 }
 
 async function downloadImage(imageUrl: string, savePath: string): Promise<void> {
-  const response = await axios.get(imageUrl, { responseType: 'stream' });
+    const response = await axios.get(imageUrl, { responseType: 'stream' });
 
-  await pipeline(response.data, createWriteStream(savePath));
+    await pipeline(response.data, createWriteStream(savePath));
 }
 
 async function searchAlbums(artist?: string, album?: string) {
-  const queryParts = [];
-  if(artist){
-    queryParts.push(`artist:"${artist}"`);
-  }
-  if(album) {
-    queryParts.push(`album:"${album}"`);
-  }
-  queryParts.push('primarytype:"album"');
+    const queryParts = [];
+    if (artist) {
+        queryParts.push(`artist:"${artist}"`);
+    }
+    if (album) {
+        queryParts.push(`album:"${album}"`);
+    }
+    queryParts.push('primarytype:"album"');
 
-  const result = await mbApi.search('release-group', { query: queryParts.join(' and '), limit: 100 });
-  return result['release-groups'];
+    const result = await mbApi.search('release-group', {
+        query: queryParts.join(' and '),
+        limit: 100,
+    });
+    return result['release-groups'];
 }
 
 function getDisplayName(group: any): string {
-  const artist = group['artist-credit']?.[0]?.name || 'Unknown Artist';
-  return `${group.title} - ${artist}`;
+    const artist = group['artist-credit']?.[0]?.name || 'Unknown Artist';
+    return `${group.title} - ${artist}`;
 }
 
 async function getCoverArt(releaseId: string): Promise<string | undefined> {
-  try {
-    const art = await coverArtApi.getReleaseGroupCovers(releaseId);
-    const front = art.images?.find(
-      (img: any) =>
-        img.front === true || img.types?.includes('Front')
-    );
+    try {
+        const art = await coverArtApi.getReleaseGroupCovers(releaseId);
+        const front = art.images?.find(
+            (img: any) => img.front === true || img.types?.includes('Front'),
+        );
 
-    return front?.thumbnails.large;
-  } catch(error) {
-    console.log(error);
-    return undefined;
-  }
+        return front?.thumbnails.large;
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
 }
 
-async function handleSearch(artistQuery?: string, albumQuery?: string): Promise<void> {
-  if(!artistQuery && !albumQuery) {
-    console.log('Please provide an artist or an album')
-    return
-  }
-  const releaseGroups = await searchAlbums(artistQuery, albumQuery);
+async function handleSearch(
+    artistQuery?: string,
+    albumQuery?: string,
+    wishlist?: boolean,
+): Promise<void> {
+    if (!artistQuery && !albumQuery) {
+        console.log('Please provide an artist or an album');
+        return;
+    }
+    const releaseGroups = await searchAlbums(artistQuery, albumQuery);
 
-  if (!releaseGroups.length) {
-    console.log('No albums found.');
-    return;
-  }
+    if (!releaseGroups.length) {
+        console.log('No albums found.');
+        return;
+    }
 
-  const { selectedGroupId } = await inquirer.prompt({
-    type: 'list',
-    name: 'selectedGroupId',
-    message: 'Select an album:',
-    choices: releaseGroups.map((group) => ({
-      name: getDisplayName(group),
-      value: group.id,
-    })),
-  });
+    const { selectedGroupId } = await inquirer.prompt({
+        type: 'list',
+        name: 'selectedGroupId',
+        message: 'Select an album:',
+        choices: releaseGroups.map(group => ({
+            name: getDisplayName(group),
+            value: group.id,
+        })),
+    });
 
-  const selectedGroup = releaseGroups.find(g => g.id === selectedGroupId);
+    const selectedGroup = releaseGroups.find(g => g.id === selectedGroupId);
 
-  const title = selectedGroup?.title || 'Unknown';
-  const artist = selectedGroup?.['artist-credit']?.[0]?.name || 'Unknown';
-  const date = selectedGroup?.['first-release-date'] || 'Unknown';
-  const coverArtUrl = await getCoverArt(selectedGroupId);
+    const title = selectedGroup?.title || 'Unknown';
+    const artist = selectedGroup?.['artist-credit']?.[0]?.name || 'Unknown';
+    const date = selectedGroup?.['first-release-date'] || 'Unknown';
+    const coverArtUrl = await getCoverArt(selectedGroupId);
 
-  const collection = readCollection();
+    const collection = readCollection();
 
-  const song: Record = {
-    artist: selectedGroup?.['artist-credit']?.[0]?.name || 'Unknown',
-    title: selectedGroup?.title || 'Unknown',
-    publishedAt: selectedGroup?.['first-release-date'] || 'Unknown',
-    releaseID: selectedGroup?.id,
-    id: collection.metadata.lastUsedID + 1,
-  }
+    const song: Record = {
+        artist: selectedGroup?.['artist-credit']?.[0]?.name || 'Unknown',
+        title: selectedGroup?.title || 'Unknown',
+        publishedAt: selectedGroup?.['first-release-date'] || 'Unknown',
+        releaseID: selectedGroup?.id,
+        id: collection.metadata.lastUsedID + 1,
+    };
 
-  if(coverArtUrl) {
-    await downloadImage(coverArtUrl, `./public/covers/${song.id}.jpg`)
-  }
-  collection.records.push(song);
+    if (coverArtUrl) {
+        await downloadImage(coverArtUrl, `./public/covers/${song.id}.jpg`);
+    }
 
-  collection.metadata.lastUsedID = song.id
+    if (wishlist) {
+        collection.wishlist.push(song);
+    } else {
+        collection.records.push(song);
+    }
 
-  saveCollection(collection);
+    collection.metadata.lastUsedID = song.id;
 
-  console.log(JSON.stringify({
-    artist,
-    album: title,
-    year: date.split('-')[0],
-    coverURL:coverArtUrl,
-  }, null, 2));
+    saveCollection(collection);
+
+    console.log(
+        JSON.stringify(
+            {
+                artist,
+                album: title,
+                year: date.split('-')[0],
+                coverURL: coverArtUrl,
+            },
+            null,
+            2,
+        ),
+    );
 }
 
 program
-  .name('vinyls')
-  .description('Search for albums and show artist, date, and cover')
+    .name('vinyls')
+    .description('Search for albums and show artist, date, and cover')
     .option('--artist <name>', 'Artist name')
-  .option('--album <name>', 'Album title')
-  .action(async (options) => {
-    const { artist, album } = options;
-    await handleSearch(artist, album);
-  });
-
+    .option('--album <name>', 'Album title')
+    .option('-w, --wishlist', 'Wishlist or nor')
+    .action(async options => {
+        const { artist, album, wishlist } = options;
+        await handleSearch(artist, album, wishlist);
+    });
 
 program.parseAsync(process.argv);
