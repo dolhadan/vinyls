eab4b42d555d9dce821c031983505254dd298ba8
dandolha <dolha.dan@gmail.com>
2025-04-02 17:39:58 +0300

    add keyboard navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import './App.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {  Virtual } from 'swiper/modules';
+import { Virtual, Keyboard, Navigation } from 'swiper/modules';
 import collection from './collections.json';
 import { useState } from 'react';
-import classNames from "classnames";
+import classNames from 'classnames';
 
 export default function App() {
-    const [recordsField, setRecordsField] = useState<'records' | 'wishlist'>('records')
+    const [recordsField, setRecordsField] = useState<'records' | 'wishlist'>('records');
     const records = collection[recordsField];
     return (
         <div className="container">
             <div className="header">
                 <div className={'segmentedControl'}>
-                    <div className={classNames({activeSegmentButton: recordsField ==='records'})} onClick={() => {
-                        setRecordsField('records')
-                    }}>owned
+                    <div
+                        className={classNames({ activeSegmentButton: recordsField === 'records' })}
+                        onClick={() => {
+                            setRecordsField('records');
+                        }}
+                    >
+                        owned
                     </div>
-                    <div className={classNames({activeSegmentButton: recordsField ==='wishlist'})} onClick={() => {
-                        setRecordsField('wishlist')
-                    }}>wishlist
+                    <div
+                        className={classNames({ activeSegmentButton: recordsField === 'wishlist' })}
+                        onClick={() => {
+                            setRecordsField('wishlist');
+                        }}
+                    >
+                        wishlist
                     </div>
                 </div>
             </div>
@@ -28,10 +36,13 @@ export default function App() {
                     effect="slide"
                     centeredSlides={true}
                     slidesPerView={1}
-                    modules={[Virtual]}
+                    initialSlide={records.length - 1}
+                    modules={[Virtual, Keyboard, Navigation]}
                     className="swiper"
                     key={recordsField}
                     virtual
+                    keyboard={{ enabled: true, onlyInViewport: false }}
+                    navigation
                 >
                     {records.map((vinyl, index) => (
                         <SwiperSlide
